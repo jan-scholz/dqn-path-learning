bef620a4cd5fa74ae1f1c3f7b843cee5ef3c10e8
Jan Scholz <jscholz.contact@gmail.com>
2025-07-24 15:11:26 -0400

    add state transitions

--- src/dqn_maze/maze_environment.py
+++ src/dqn_maze/maze_environment.py
@@ -4,6 +4,9 @@ import numpy as np
 class Maze:
     def __init__(self):
         self.grid = None
+        self.rows, self.cols = 0, 0
+        self.action_names = ["Up", "Down", "Left", "Right"]
+        self.actions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
     def _initialize_grid(self, grid_list):
         self.grid = np.array(grid_list)
@@ -13,6 +16,21 @@ class Maze:
             raise ValueError("Grid must contain a start position 'S'")
         if not np.any(self.grid == "G"):
             raise ValueError("Grid must contain a start position 'G'")
+
+    def is_valid_state(self, state):
+        row, col = state
+        if 0 <= row < self.rows and 0 <= col < self.cols:
+            return self.grid[row, col] != "X"
+        return False
+
+    def get_next_state(self, state, action_idx):
+        row, col = state
+        d_row, d_col = self.actions[action_idx]
+        next_state = (row + d_row, col + d_col)
+
+        if self.is_valid_state(next_state):
+            return next_state
+        return state
 
     def from_str(self, maze_str):
         rows = [r.split() for r in maze_str.strip().splitlines()]
@@ -30,6 +48,8 @@ class Maze:
         self._initialize_grid(grid_list)
 
     def __str__(self):
+        if self.grid is None:
+            return "Grid not initialized."
         s = f"Grid ({self.rows}x{self.cols})\n"
         s += "\n".join([" ".join(r) for r in self.grid])
         return s
@@ -47,6 +67,8 @@ def main():
     maze = Maze()
     maze.from_str(maze_str)
     print(maze)
+    print()
+    print(maze.get_next_state((0, 0), 1))
 
 
 if __name__ == "__main__":
