001f771eeadbe63db36edf1621e67f29af152b53
Jan Scholz <jscholz.contact@gmail.com>
2025-07-24 12:11:57 -0400

    add basic maze class

--- src/dqn_maze/maze_environment.py
+++ src/dqn_maze/maze_environment.py
@@ -1,5 +1,52 @@
+import numpy as np
+
+
+class Maze:
+    def __init__(self):
+        self.grid = None
+
+    def _initialize_grid(self, grid_list):
+        self.grid = np.array(grid_list)
+        self.rows, self.cols = self.grid.shape
+
+        if not np.any(self.grid == "S"):
+            raise ValueError("Grid must contain a start position 'S'")
+        if not np.any(self.grid == "G"):
+            raise ValueError("Grid must contain a start position 'G'")
+
+    def from_str(self, maze_str):
+        rows = [r.split() for r in maze_str.strip().splitlines()]
+        ncols = len(rows[0])
+        grid_list = []
+
+        for row in rows:
+            if len(row) != ncols:
+                raise ValueError(f"Row length {len(row)} != {ncols}")
+            grid_list.append(row)
+
+        self.from_list_of_lists(grid_list)
+
+    def from_list_of_lists(self, grid_list):
+        self._initialize_grid(grid_list)
+
+    def __str__(self):
+        s = f"Grid ({self.rows}x{self.cols})\n"
+        s += "\n".join([" ".join(r) for r in self.grid])
+        return s
+
+
 def main():
-    print("hello")
+    maze_str = """
+        S . . X G
+        . X . X .
+        . . . . .
+        X X . X .
+        . . . . .
+    """
+
+    maze = Maze()
+    maze.from_str(maze_str)
+    print(maze)
 
 
 if __name__ == "__main__":
